a853bdadf4e3b157317e8b1c024975f42a71ceba
kikilizhm <kikilizhm@163.com>
2020-01-03 23:39:27 +0800

    增加备案信息

--- web/cgi.c
+++ web/cgi.c
@@ -38,7 +38,7 @@ void dis_rest(void)
         http_buf=(char *)malloc(len+1);
         memset((char *)http_buf,0x0,len+1);
         fgets((char *)http_buf,len+1,stdin);
-        printf("[%s]\r\n",http_buf);
+        //printf("[%s]\r\n",http_buf);
 
         str = strstr(http_buf,"=");
         if(str)
